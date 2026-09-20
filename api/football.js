fa66a9734f00b868f9efb829211d6bc128ce249b
Michealking-2006 <michealkingg2006@gmail.com>
2026-09-20 03:02:02 +0100

    Remove exposed football API credential from source

--- api/football.js
+++ api/football.js
@@ -1,6 +1,7 @@
 const API_BASE = "https://v3.football.api-sports.io";
 
-const API_KEY = "c3e4c14be7c1b0cb011eefd4ab4ddeaf";
+// Paste your Football API key between the quotes before deploying.
+const API_KEY = "";
 
 const ALLOWED_ENDPOINTS = new Set([
   "leagues",
