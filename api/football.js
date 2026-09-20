e218079cb7968f9245b9a6e362808dfab9635b78
Michealkingg2006 <Michealkingg2006@gmail.com>
2026-09-20 03:24:37 +0100

    update football.js

--- api/football.js
+++ api/football.js
@@ -1,7 +1,7 @@
 const API_BASE = "https://v3.football.api-sports.io";
 
 // Paste your Football API key between the quotes before deploying.
-const API_KEY = "";
+const API_KEY = "c3e4c14be7c1b0cb011eefd4ab4ddeaf";
 
 const ALLOWED_ENDPOINTS = new Set([
   "leagues",
